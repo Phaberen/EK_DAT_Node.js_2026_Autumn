0f1ca345210ad5ce3efc75aabb1fafb8b789c57d
Phaberen <Magnusphaberen@gmail.com>
2026-09-11 10:31:24 +0200

    updated progress curriculum

---
+++ 03_Animals_API/REST_API_Design.md
@@ -0,0 +1,9 @@
+REST API CONVENTIONS
+
+1. Naming (Plural), ie "/animals" and not "/animal"
+
+2. Ordering
+
+
+GET /animals
+GET /animals/{id}
--- 03_Animals_API/app.js
+++ 03_Animals_API/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // imports express
+const app = express(); // instantiates express
 
 // In-memory "database": an array of objects.
 // Array because /animals must return a list, main action is GET
@@ -16,17 +16,26 @@ app.get('/animals', (req, res) => {
 
 // GET /animals/:id  -> one animal
 app.get('/animals/:id', (req, res) => {
-    // req.params values are always strings, so convert before comparing with ===
+
+    // req.params values are always strings, so convert to number before comparing with ===
     const id = Number(req.params.id);
 
+    // 
     const animal = animals.find((animal) => animal.id === id);
 
     if (!animal) {
-        return res.status(404).send({ message: `No animal with id ${id}` });
+        res.status(404).send({ message: `No animal with id ${id}` });
+        return; // exit the function early if no animal is found
     }
 
     res.send({ data: animal });
 });
+
+
+// 2xx OK
+// 3xx Redirect
+// 4xx Client-side error
+// 5xx Server-side error
 
 const PORT = 8080;
 app.listen(PORT, () => {
